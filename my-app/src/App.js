b983ab9ebfb753bf225009710c07a64761821fa1
MaxSciuclin <maxiambraci@gmail.com>
2023-04-13 21:41:22 -0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,16 +8,20 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 // this is a functional component that will render the components
 function App() {
   return (
   // this is router that will handle the routes
   <Router>
      <Navbar />
-     {/* <Hero/> */}
+     
+
+     
      {/* // this is the Routes component that will handle the routes that link to pages */}
     <Routes>
       <Route path="/" element={<Hero/>}/>
+      <Route path="/navbar" element={<Navbar/>}/>
       <Route path="/hero" element={<Hero/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/projects" element={<Projects/>}/>
